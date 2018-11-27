331d051642c4019519ecd7bf3b3cb27231626638
brooke perry and Michael Brown <michael@>
2018-11-27 11:57:41 -0800

    update spec files for new syntax

--- spec/riddle_integration_spec.rb
+++ spec/riddle_integration_spec.rb
@@ -2,3 +2,15 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the riddle path', {:type => :feature}) do
+
+  it('process user entry and returns a success or failure page') do
+    visit('/')
+    fill_in('answer0', :with => "human")
+    fill_in('answer1', :with => "river")
+    fill_in('answer2', :with => "map")
+    click_button('Test my knowledge!')
+    expect(page).to have_content("Success!")
+  end
+end
--- spec/riddle_spec.rb
+++ spec/riddle_spec.rb
@@ -3,16 +3,16 @@ require 'pry'
 require 'riddle'
 
 describe('#riddle') do
-  it('allows you to change your answer') do
-    test = CurrentGame.new()
-    expect(test.answer = "Human").to(eq("Human"))
+  it('allows you to change your answers') do
+    test_game = CurrentGame.new()
+    expect(test_game.answers[0] = "Human").to(eq("Human"))
   end
 
-  it('return true or false if answer matches value to riddle key') do
-    test = CurrentGame.new()
-    expect(test.answer_riddle(0)).to(eq(false))
-    test.answer[0] = "Human"
-    expect(test.answer_riddle(0)).to(eq(true))
+  it('returns the number of correct answers') do
+    test_game = CurrentGame.new()
+    expect(test_game.answers_riddle).to(eq(0))
+    test_game.answers[0] = "Human"
+    expect(test_game.answers_riddle).to(eq(1))
   end
 
 
