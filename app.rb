f56705e26e43945ab0b1dd7b06352957ee08a2ff
brooke perry and Michael Brown <michael@>
2018-11-27 11:42:25 -0800

    create UI and simple logic for results page

--- app.rb
+++ app.rb
@@ -4,5 +4,23 @@ also_reload('lib/**/*.rb')
 require('./lib/riddle')
 
 get ('/') do
+  game = CurrentGame.new()
+  @riddle0 = game.riddles.values[0]
+  @riddle1 = game.riddles.values[1]
+  @riddle2 = game.riddles.values[2]
   erb(:input)
+end
+
+post('/result') do
+  game = CurrentGame.new()
+  game.answers[0] = params.fetch("answer0")
+  game.answers[1] = params.fetch("answer1")
+  game.answers[2] = params.fetch("answer2")
+
+  if game.answers_riddle < game.riddles.values.length
+    erb(:failure)
+  else
+    erb(:success)
+  end
+
 end
